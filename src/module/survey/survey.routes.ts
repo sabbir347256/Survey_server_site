5fb8d30474cb159d94208e9b6765a9211a11d942
Sabbir Ahmmed <srgamingzone71@gmail.com>
2026-06-27 18:05:24 +0600

    some code delet

--- src/module/survey/survey.routes.ts
+++ src/module/survey/survey.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { checkAuth } from "../middleware/auth.middleware";
 import { Role } from "../user/user.interface";
-import geoBlockMiddleware from "../middleware/geoblock.midleware";
 import { surveyController } from "./survey.controller";
 
 
 const router = Router();
 
-router.get('/get-all-surveys',checkAuth(Role.EMPLOYEE),geoBlockMiddleware, surveyController.getAllSurveys );
+router.get('/get-all-surveys',checkAuth(Role.EMPLOYEE), surveyController.getAllSurveys );
 router.post('/lootwalls', surveyController.handleLootwallsCallback);
 
 export const SurveyRouter = router;
