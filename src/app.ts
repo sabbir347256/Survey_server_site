02944c884066976c76d86d6bea58b0cf73f78ec5
Sabbir Ahmmed <srgamingzone71@gmail.com>
2026-06-27 18:08:18 +0600

    app update

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "https://survey-client-site.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
