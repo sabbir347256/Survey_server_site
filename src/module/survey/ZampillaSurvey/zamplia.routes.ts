b71897994a70c6d5da515f2a0b03a5fbbc96f7d3
Sabbir Ahmmed <srgamingzone71@gmail.com>
2026-07-01 17:28:26 +0600

    geoblock add

--- src/module/survey/ZampillaSurvey/zamplia.routes.ts
+++ src/module/survey/ZampillaSurvey/zamplia.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import {  startSurvey, syncSurveys } from './Zamplia.controller';
+import geoBlockMiddleware from '../../middleware/geoblock.midleware';
 
 const router = Router();
 
-router.get('/surveys/sync', syncSurveys);
+router.get('/surveys/sync',geoBlockMiddleware, syncSurveys);
 router.post('/surveys/start', startSurvey);
 // router.get('/surveys/callback', handleExitCallback);
 
